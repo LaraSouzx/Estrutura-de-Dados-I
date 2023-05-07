6d4a6611e1666ebc5ceb886eb3057c912aa3b5e1
Lara Souza <larasouzadev@gmail.com>
2023-05-07 14:52:30 -0300

    Atualização da classe Principal

--- ProjetoLista/src/Principal.java
+++ ProjetoLista/src/Principal.java
@@ -2,18 +2,30 @@
 public class Principal {
 
 	public Principal() {
-		LSE lista = new LSE();
+		LDE lista = new LDE();
+		lista.inserirInicio(33);
+		lista.inserirFinal(12);
+		lista.inserirFinal(15);
+		lista.inserirFinal(52);
+		lista.inserirFinal(19);
+		lista.inserirFinal(18);
+		lista.inserirPos(92, 3);
+		lista.print();
+		
+		/*LSE lista = new LSE();
 		lista.inserirInicio(22);
 		lista.inserirFinal(14);
 		lista.inserirFinal(12);
 		lista.inserirFinal(19);
 		lista.inserirFinal(21);
 		lista.inserirFinal(15);
-		lista.inserirPos(33,2);
+		lista.inserirPos(33, 2);
+		//lista.removerInicio();
+		//lista.removerFinal();
+		//lista.removerPos(3);
 		int posicao = lista.buscarValor(14);
-		System.out.println(posicao);
-		
-		//lista.print();
+		lista.print();
+		System.out.println("O valor procurado esta na posicao "+posicao);*/
 	}
 	
 	public static void main(String[] args) {
